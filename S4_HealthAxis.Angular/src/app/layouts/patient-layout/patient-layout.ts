c108c5080ba783e9ff8ef689b8e3d614029a50fe
Abhishek Upadhyay <abhishek.upadhyay@ust.com>
2026-07-27 20:56:59 +0530

    Changed CSS / UI to mobile friendly v1

--- S4_HealthAxis.Angular/src/app/layouts/patient-layout/patient-layout.ts
+++ S4_HealthAxis.Angular/src/app/layouts/patient-layout/patient-layout.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-patient-layout',
+  standalone: true, /* Required to use the imports array below */
+  imports: [RouterModule], /* Tells Angular how to read <router-outlet> and routerLink */
   templateUrl: './patient-layout.html',
   styleUrls: ['./patient-layout.css']
 })
-export class PatientLayoutComponent {
+export class PatientLayout {
   isMobileMenuOpen = false;
 
   toggleMobileMenu() {
